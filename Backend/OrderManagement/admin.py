e0d8e22dd9a10a9675e5c30302f7226365053a74
RITTIK.ROY <rittik.roy@itcinfotech.com>
2023-01-04 01:31:57 +0530

    Modified OrderManagement admin.py file

--- Backend/OrderManagement/admin.py
+++ Backend/OrderManagement/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from OrderManagement.models import OrderModel
+from OrderManagement.models import OrderModel, OrderListModel
 
 class OrderModelAdmin(admin.ModelAdmin):
     list_display= ['orderId', 'buyerProfileModelIns', 'itemIns', 'paymentStatus', 'deliveryStatus']
 
+class OrderListModelAdmin(admin.ModelAdmin):
+    list_display= ['orderListId', 'buyerProfileModelIns', 'orderIdList']
+
+
 
 admin.site.register(OrderModel, OrderModelAdmin)
+admin.site.register(OrderListModel, OrderListModelAdmin)
