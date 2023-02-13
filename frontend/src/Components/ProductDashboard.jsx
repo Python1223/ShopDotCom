ad0c769ad87dcc5cd2d06015c7e96ece5619ee3d
stuntmartial <rajarsiroy81509@gmail.com>
2023-02-13 22:02:06 +0530

    added ui

--- frontend/src/Components/ProductDashboard.jsx
+++ frontend/src/Components/ProductDashboard.jsx
@@ -49,37 +49,21 @@ const ProductDashboard= ()=>{
       <Carousel.Item>
         <img
           className="d-block w-100 a"
-          src="https://i.gadgets360cdn.com/large/big-billion-days-2022-sale_1663819658737.png"
+          src="https://i.gadgets360cdn.com/large/Flipkart-Big-Billion-Days-Sale-1200x800-1632897416071.jpg"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block w-100 a"
           src="https://pbs.twimg.com/media/EljBauEU4AAToRQ.jpg"
           alt="Second slide"
         />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
 
-      <section class="section-products">
+      <section class="section-products position">
         <div class="container-fluid">
-          <div class="row justify-content-center text-center">
-              <div class="col-md-8 col-lg-6">
-                  <div class="header">
-                      <h2>Featured Product</h2>
-                  </div>
-              </div>
-          </div>
           <div class="row">
             
           {/* <ItemInProductDashboard />
@@ -91,46 +75,77 @@ const ProductDashboard= ()=>{
           <ItemInProductDashboard />
           <ItemInProductDashboard /> */}
 
-          <div class="card col-3">
-            <img class="card-img-top" src="https://assets.ajio.com/medias/sys_master/root/20221109/61kh/636b8ebaf997ddfdbd664273/-473Wx593H-462323964-purple-MODEL.jpg" alt="Card image cap"/>
-            <div class="card-body">
-              <p class="card-text">Buy</p>
+          <div class="card col-3 back">
+            <h4 class="fonts">Male Shirts</h4>
+            <div class="row mid">
+            <img class="card-img-top noBorderR" src="https://assets.ajio.com/medias/sys_master/root/20221109/61kh/636b8ebaf997ddfdbd664273/-473Wx593H-462323964-purple-MODEL.jpg" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://img3.junaroad.com/uiproducts/19126301/pri_175_p-1673529653.jpg" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://www.muftijeans.in/media/catalog/product/cache/72ecbf8444b2c4d1e625f30416db39ce/1/_/1_mfs-11994-o-03-red_1.jpg" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://cdn.shopify.com/s/files/1/1414/2498/products/ClassicShirt_FrenchBlue1_1024x1024.jpg?v=1667207840" alt="Card image cap"/>
             </div>
           </div>
 
-          <div class="card col-3">
-            <img class="card-img-top" src="https://assets.ajio.com/medias/sys_master/root/20221109/61kh/636b8ebaf997ddfdbd664273/-473Wx593H-462323964-purple-MODEL.jpg" alt="Card image cap"/>
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="card col-3 back">
+            <h4 class="fonts">Female Shirts</h4>
+            <div class="row mid">
+            <img class="card-img-top noBorderR" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYTb4j_YQqh5D2hZr_DAAGwJZGJOl1H9s_wQ&usqp=CAU" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://img.tatacliq.com/images/i8/437Wx649H/MP000000014412833_437Wx649H_202209101828191.jpeg" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://images.bewakoof.com/t640/women-s-checks-overlap-shirt-top-300373-1656171760-1.jpg" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://images.meesho.com/images/products/92647725/rinpx_256.jpg" alt="Card image cap"/>
             </div>
           </div>
 
-          <div class="card col-3">
-            <img class="card-img-top" src="G:\ShopDotCom\ShopDotCom\frontend\pic.png" alt="Card image cap"/>
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="card col-3 back">
+            <h4 class="fonts">Jeans</h4>
+            <div class="row mid">
+            <img class="card-img-top noBorderR" src="https://imagescdn.aeo.in/img/app/brands/ae/jeans/men/Skinny_11-12-2019.jpg?auto=format" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://images.lee.com/is/image/Lee/2076456-ALT8?$KDP-MEDIUM$" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://www.petermillar.com/dw/image/v2/BHFJ_PRD/on/demandware.static/-/Sites-pm-master-catalog/default/dw8358ccfe/ME0XB90FB_SWB_ALT_A.jpg?sw=478&sh=649" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://www.politix.com.au/dw/image/v2/ABBA_PRD/on/demandware.static/-/Sites-politix-master-catalog/default/dwd2dbdc2c/images/hires/Summer%202022/B3/B3%20Day%203/Re-Shoot/yd11-dkindigo-6.jpg?sw=2000&sh=2000&sm=fit" alt="Card image cap"/>
             </div>
           </div>
 
-          <div class="card col-3">
-            <img class="card-img-top" src="..." alt="Card image cap"/>
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="card col-3 back">
+            <h4 class="fonts">Sweaters</h4>
+            <div class="row mid">
+            <img class="card-img-top noBorderR" src="https://5.imimg.com/data5/SW/WE/MY-11559983/knitted-mens-sweaters-250x250.jpg" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://imagescdn.planetfashion.in/img/app/product/7/746139-8403776.jpg?auto=format" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/21334354/2022/12/28/99ffe786-a30a-47ad-b484-60bb0c949c961672229892135AMERICANEAGLEOUTFITTERSMenTanWhiteFairIslePrintedPullover1.jpg" alt="Card image cap"/>
+            <img class="card-img-top noBorderR" src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/21334356/2022/12/28/327b7dc0-1d00-4bbc-918e-80af97cd21ce1672229884013AMERICANEAGLEOUTFITTERSMenBurgundyWhiteStripedStripedPullove1.jpg" alt="Card image cap"/>
             </div>
+            
+          </div>
+          {/* <div class="row justify-content-center text-center">
+              <div class="col-md-8 col-lg-6">
+                  <div class="header">
+                      <h2>Featured Product</h2>
+                  </div>
+              </div>
+          </div> */}
+
+          </div>
+          <div class="row back2">
+          <ItemInProductDashboard />
+          <ItemInProductDashboard />
+          <ItemInProductDashboard />
+          <ItemInProductDashboard />
+          <ItemInProductDashboard />
+          <ItemInProductDashboard />
+          <ItemInProductDashboard />
+          <ItemInProductDashboard />
+          <ItemInProductDashboard />
+          <ItemInProductDashboard />
+          <ItemInProductDashboard />
+          <ItemInProductDashboard />
           </div>
 
-
-          </div>
           <div class="row">
-          <ItemInProductDashboard />
-          <ItemInProductDashboard />
-          <ItemInProductDashboard />
-          <ItemInProductDashboard />
-          <ItemInProductDashboard />
-          <ItemInProductDashboard />
-          <ItemInProductDashboard />
-          <ItemInProductDashboard />
+            <div class="col-4">
+
+            </div>
+
           </div>
+
           </div>
         
       </section>
