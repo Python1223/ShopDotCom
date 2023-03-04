b08e7d6dff6c1cca4450992e1d475db3bc86614e
stuntmartial <rajarsiroy81509@gmail.com>
2023-03-04 20:12:06 +0530

    Started Add to Cart implementation

--- frontend/src/Components/BuyerItemView.jsx
+++ frontend/src/Components/BuyerItemView.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
 import Item from './Item'
+import URLS from '../urls'
+
+const CartUrl= URLS.Backend_BASE_URL+ URLS.Cart
 
 const BuyerItemView= ()=> {
 
   const itemId= useParams().itemId
+
+  const addItemToCart=(event)=>{
+    event.preventDefault()
+    
+    console.log(event.target.name)
+    
+
+    console.log('Data-> ', signupData, 'url-> ', CartUrl)
+
+    // Include Access Token
+    axios({method: 'patch', url: CartUrl, data: {'itemId': itemId}}).then(
+    ((response)=> console.log(response)),
+    ((error)=> console.log(error))
+    )
+
+  }
+
+
   return(
     <React.Fragment>
     <Item itemId= {itemId} k2= {4}></Item>
-    <button>Add to Cart</button>
+    <button name="AddItemToCartButton" type="submit" onClick={addItemToCart}>Add to Cart</button>
     <button>Buy Now</button>
     </React.Fragment>
   )
